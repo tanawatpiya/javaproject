25a52e9ff037dd41e15823359a5f16b506355943
TETE3138 <tewichtete31@hotmail.com>
2015-11-11 23:23:09 +0700

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,23 @@
+public class Question {
+	int questionID;
+	String questionText;
+	String answerText;
+	QuestionResult result = QuestionResult.NotAnswered;
+	int triedCount =0 ;
+	
+	Question (String question, String answer){
+		
+	}
+	ArrayList<char> getAnswerCharList(){
+		
+	}
+	int getAnswerCharCount(){
+		
+	}
+	ArrayList<char> getChoice(){
+		
+	}
+	QuestionResult submitAndswer(char c){
+		
+	}
+ }
