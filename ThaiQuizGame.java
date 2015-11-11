2af2a0adf6f85a9da83607cae4393c8a48a2cb4c
tanawatpiya <tanawat.piya@hotmail.com>
2015-11-12 00:02:25 +0700

    Moss

---
+++ ThaiQuizGame.java
@@ -0,0 +1,17 @@
+class ThaiQuizGame {
+  private int score;
+  
+  public static void main(String[] args) {
+  
+  }
+  private void showTitleForm() {
+  
+  }
+  private void showQuestionForm() {
+  
+  }
+  private void showScoreForm () {
+  
+  }
+  
+}
